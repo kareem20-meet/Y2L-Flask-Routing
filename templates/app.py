179c5f10f2a4e403821215ee7d77042743e03e03
myname21-meet <myname21@meet.mit.edu>
2019-11-07 11:45:52 -0500

    CIAO

--- templates/app.py
+++ templates/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, url_for, render_template
 app = Flask(__name__)
  
-@app.route('/home.html')
+@app.route('/')
 def index():
     return render_template("home.html")
  
-@app.route('/store')
-def store():
+@app.route('/hello')
+def hello():
     return render_template("store.html")
  
 app.run(debug = True)
